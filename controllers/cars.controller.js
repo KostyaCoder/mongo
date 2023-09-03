63138f97c212469690a839e8cc49ac1470001d85
Kostya <forgit266@gmail.com>
2023-09-03 12:33:48 +0000

    Add CRUD

--- controllers/cars.controller.js
+++ controllers/cars.controller.js
@@ -1,10 +1,12 @@
 const createHttpError = require("http-errors");
+const { Car } = require("../models");
 
 module.exports.createCar = async (req, resp, next) => {
-  const { body } = req;
-
   try {
-    resp.status(201).send({ data: body });
+    const { body } = req;
+    const car = await Car.create(body);
+
+    resp.status(201).send({ data: car });
   } catch (error) {
     next(error);
   }
@@ -12,37 +14,52 @@ module.exports.createCar = async (req, resp, next) => {
 
 module.exports.getCars = async (req, resp, next) => {
   try {
-    resp.status(200).send({ data: [] });
+    const cars = await Car.find();
+
+    resp.status(200).send({ data: cars });
   } catch (error) {
     next(error);
   }
 };
 
 module.exports.getCar = async (req, resp, next) => {
-  const { carId } = req;
-
   try {
-    resp.status(200).send({ data: carId });
+    const {
+      params: { carId },
+    } = req;
+
+    const car = await Car.findById(carId);
+
+    resp.status(200).send({ data: car });
   } catch (error) {
     next(error);
   }
 };
 
 module.exports.updateCar = async (req, resp, next) => {
-  const { carId } = req;
-
   try {
-    resp.status(200).send({ data: carId });
+    const {
+      params: { carId },
+      body,
+    } = req;
+
+    const car = await Car.findByIdAndUpdate(carId, body, { new: true });
+
+    resp.status(200).send({ data: car });
   } catch (error) {
     next(error);
   }
 };
 
 module.exports.deleteCar = async (req, resp, next) => {
-  const { carId } = req;
-
   try {
-    resp.status(200).send({ data: carId });
+    const {
+      params: { carId },
+    } = req;
+
+    const car = await Car.findByIdAndDelete(carId);
+
+    resp.status(200).send({ data: car });
   } catch (error) {
     next(error);
   }
