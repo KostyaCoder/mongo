fd527693391ccf87667e2a6eb2769228edb3c747
Kostya <forgit266@gmail.com>
2023-09-05 16:07:06 +0000

    Add controler review

---
+++ controllers/reviews.controller.js
@@ -0,0 +1,116 @@
+const createHttpError = require("http-errors");
+const ReviewService = require("../services/review.service");
+const { Review } = require("../models");
+
+module.exports.createReview = async (req, res, next) => {
+  try {
+    const { car, body } = req;
+
+    const review = await ReviewService.createReview({
+      body,
+      car,
+    });
+
+    res.status(201).send({ data: review });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getAllReviews = async (req, res, next) => {
+  try {
+    const reviews = await ReviewService.findReviews();
+
+    res.send({ data: reviews });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getCarReviews = async (req, res, next) => {
+  try {
+    const { car } = req;
+
+    const carReviews = await ReviewService.findReviews(
+      {
+        car: car._id,
+      },
+      "-__v"
+    );
+
+    res.send({ data: carReviews });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getReview = async (req, res, next) => {
+  try {
+    const {
+      params: { reviewId },
+      car: { _id: carId },
+    } = req;
+
+    const review = await Review.findOne({
+      _id: reviewId,
+      car: carId,
+    });
+
+    if (!review) {
+      return next(createHttpError(404, "Review not found"));
+    }
+
+    res.send({ data: review });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.updateReview = async (req, res, next) => {
+  try {
+    const {
+      car: { _id: carId },
+      params: { reviewId },
+      body,
+    } = req;
+
+    const updatedReview = await Review.findOneAndUpdate(
+      {
+        _id: reviewId,
+        car: carId,
+      },
+      body,
+      { new: true }
+    );
+
+    if (!updatedReview) {
+      return next(createHttpError(404, "Review not found"));
+    }
+
+    res.send({ data: updatedReview });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.deleteReview = async (req, res, next) => {
+  try {
+    const {
+      car: { _id: carId },
+      params: { reviewId },
+    } = req;
+
+    const review = await Review.findOneAndDelete({
+      _id: reviewId,
+      car: carId,
+    });
+
+    if (!review) {
+      return next(createHttpError(404, "Review not found"));
+    }
+
+    res.send({ data: review });
+  } catch (error) {
+    next(error);
+  }
+};
