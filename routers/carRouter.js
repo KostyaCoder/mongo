50a80c71381f7e70eb259b239f63a8ee8c38de93
Kostya <forgit266@gmail.com>
2023-09-05 16:06:14 +0000

    Add router review

--- routers/carRouter.js
+++ routers/carRouter.js
@@ -1,10 +1,14 @@
 const carRouter = require("express").Router();
 const carController = require("../controllers/cars.controller");
+const { findCar } = require("../middlewares/cars.mw");
+const reviewRouter = require("./reviewRouter");
 
 carRouter.post("/", carController.createCar);
 carRouter.get("/", carController.getCars);
 carRouter.get("/:carId", carController.getCar);
 carRouter.put("/:carId", carController.updateCar);
 carRouter.delete("/:carId", carController.deleteCar);
+
+carRouter.use("/:carId/reviews", findCar, reviewRouter);
 
 module.exports = carRouter;
