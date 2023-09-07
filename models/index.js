9eb574e5a8c35bfb758ea3fd6e8a6544a08ab83a
Kostya <forgit266@gmail.com>
2023-09-07 10:49:19 +0000

    Rename dealer

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const { DB_URL } = require("../constants");
 const Car = require("./car");
-const carDealer = require("./carDealer");
+const Dealer = require("./carDealer");
 const Review = require("./review");
 
 connectToDb().catch((err) => console.log(err));
@@ -10,4 +10,4 @@ async function connectToDb() {
   await mongoose.connect(DB_URL);
 }
 
-module.exports = { Car, carDealer, Review };
+module.exports = { Car, Dealer, Review };
